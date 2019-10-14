7cde8f856068692719b076cb52f56e7722f2b63d
MeronZerihun <merry393zerihun@gmail.com>
2019-10-14 20:05:57 +0300

    Fixes on models

--- tena-api/models/User.js
+++ tena-api/models/User.js
@@ -9,7 +9,8 @@ let userSchema = new Schema({
         phoneNo: {type: String, required: true},
         email: {type: String, required: true, unique: true},
         password: {type: String, required: true},
-        role: {type: String, required: true}
+        role: {type: String, required: true},
+        status: {type: String, default: 'open'}
     }
 );
 
