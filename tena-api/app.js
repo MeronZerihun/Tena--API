4f1a5b61c0dff1cbe8d8fd54ccb524bcaeb60fde
MeronZerihun <merry393zerihun@gmail.com>
2019-11-02 11:34:40 +0300

    Minor fixes

--- tena-api/app.js
+++ tena-api/app.js
@@ -22,7 +22,7 @@ var dbConnect = require('./config/connectDb');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
