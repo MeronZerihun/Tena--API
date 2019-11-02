4e3dfd97f84bde62ff7d48d28a45968d2f579178
MeronZerihun <merry393zerihun@gmail.com>
2019-11-02 11:27:38 +0300

    Fixes on app

--- tena-api/app.js
+++ tena-api/app.js
@@ -62,7 +62,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500).json('this route is not defined');
+  res.status(err.status || 500).json({error: 'this route is not defined'});
   //res.render('error');
 });
 
